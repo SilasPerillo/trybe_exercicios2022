d4cf1b87f9438a5c436b59542ebc6055377c0a83
SilasPerillo <silasperillo@gmail.com>
2022-04-06 20:43:24 -0300

    5º item do exercicio

--- 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/01dia-JavaScript-Primeiros_passos/exercicio/script.js
+++ 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/01dia-JavaScript-Primeiros_passos/exercicio/script.js
@@ -1,5 +1,5 @@
 // 1º item do exercicio
-const a = 3;
+const a = -3;
 const b = 6;
 const c = 9;
 
@@ -42,3 +42,8 @@ if (a > 0) {
 
 //5º item do exercicio
 //Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+
+let somaAngulo = a + b + c;
+let anguloPositivo = a > 0 && b > 0 && c > 0;
+
+console.log(anguloPositivo);
