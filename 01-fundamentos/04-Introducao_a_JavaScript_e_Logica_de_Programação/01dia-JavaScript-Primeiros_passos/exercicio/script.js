3ed850a91e7f0686486b29254be262aa979371f5
SilasPerillo <silasperillo@gmail.com>
2022-04-06 19:29:08 -0300

    2º item do exercicio

--- 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/01dia-JavaScript-Primeiros_passos/exercicio/script.js
+++ 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/01dia-JavaScript-Primeiros_passos/exercicio/script.js
@@ -1,3 +1,4 @@
+// 1º item do exercicio
 const a = 3;
 const b = 6;
 
@@ -6,3 +7,12 @@ console.log('Subtração a - b =', a - b);
 console.log('Multiplicação a * b =', a * b);
 console.log('Divisão a / b =', a / b);
 console.log('Módulo a % b =', a % b);
+
+//2º item do exercicio
+//Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+
+if (a > b){
+    console.log('O numero contido em "a" =', a, 'é maior')
+} else {
+    console.log('O numero contido em "b" =', b, 'é maior')
+}
