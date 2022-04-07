3aa2a7f8079dc4598d06701f3dfb79f4bc75fbfa
SilasPerillo <silasperillo@gmail.com>
2022-04-07 18:06:12 -0300

    Exercicio 4.2 - item 3

--- 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/02dia-JavaScript-Array_e_loop_For/exercicio/script.js
+++ 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/02dia-JavaScript-Array_e_loop_For/exercicio/script.js
@@ -1,3 +1,4 @@
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 /*
@@ -5,14 +6,31 @@ for (var imprimir of numbers) {
     
     console.log(imprimir);
 }
-
-*/
+//2º item
 
 let soma = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
    soma += numbers[index];
-   //console.log(numbers[index])
 }
 
 console.log(soma);
+
+*/
+
+//3º item
+//Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+let somaValores = 0
+let valorIndex = 0
+let valorMedia = 0
+
+for (let index = 0; index < numbers.length; index += 1) {
+    somaValores += numbers[index];
+    //console.log(somaValores);
+    valorIndex += 1 ;
+ }
+
+ valorMedia = somaValores / valorIndex;
+ console.log(valorMedia);
+
