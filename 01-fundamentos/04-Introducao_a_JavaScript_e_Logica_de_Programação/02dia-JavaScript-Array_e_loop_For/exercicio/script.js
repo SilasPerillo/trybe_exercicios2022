eec3559839e48eae607df38b702a77f770ce8faa
SilasPerillo <silasperillo@gmail.com>
2022-04-07 17:19:59 -0300

    Exercicio 4.2 - item 2

--- 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/02dia-JavaScript-Array_e_loop_For/exercicio/script.js
+++ 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/02dia-JavaScript-Array_e_loop_For/exercicio/script.js
@@ -1,6 +1,18 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+/*
 for (var imprimir of numbers) {
     
     console.log(imprimir);
 }
+
+*/
+
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+   soma += numbers[index];
+   //console.log(numbers[index])
+}
+
+console.log(soma);
