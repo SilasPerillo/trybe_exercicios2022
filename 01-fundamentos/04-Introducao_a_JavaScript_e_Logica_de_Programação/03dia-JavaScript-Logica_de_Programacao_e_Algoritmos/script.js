d4fc0d11a8ffac6d25e9dc45b5d5f21463919d12
SilasPerillo <silasperillo@gmail.com>
2022-04-08 19:13:43 -0300

    exercicio/4.3 - 2 item

--- 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/03dia-JavaScript-Logica_de_Programacao_e_Algoritmos/script.js
+++ 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/03dia-JavaScript-Logica_de_Programacao_e_Algoritmos/script.js
@@ -1,6 +1,6 @@
+/*
 //1 item
 //O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
-/*
 let num = 10;
 let numFatorado = 1;
 
@@ -9,7 +9,6 @@ for (let index = num; index >= 1; index -= 1) {
 }
 
 console.log(numFatorado);
-*/
 //2 item
 //Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 
@@ -21,4 +20,31 @@ for (let index = 0; index < word.length; index += 1) {
 }
 
 console.log(wordInvert);
-console.log(word.length);
+
+*/
+
+//3 item
+//Considere o array de strings abaixo: Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = array[0];
+let menorPalavra = array[0];
+
+
+for (let index = 0;  index < array.length; index += 1) {
+    if (array[index].length > maiorPalavra.length) {
+        maiorPalavra = array[index];
+    }
+}
+
+
+for (let index = 0;  index < array.length; index += 1) {
+    if (array[index].length < menorPalavra.length) {
+        menorPalavra = array[index];
+    }
+}
+
+console.log(maiorPalavra);
+console.log(menorPalavra);
+
+
