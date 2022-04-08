b63f144a409acf067d9757576ebf5f19bba7d9c2
SilasPerillo <silasperillo@gmail.com>
2022-04-08 18:54:16 -0300

    exercicio/4.3 - 2 item

--- 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/03dia-JavaScript-Logica_de_Programacao_e_Algoritmos/script.js
+++ 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/03dia-JavaScript-Logica_de_Programacao_e_Algoritmos/script.js
@@ -1,6 +1,6 @@
 //1 item
 //O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
-
+/*
 let num = 10;
 let numFatorado = 1;
 
@@ -9,3 +9,16 @@ for (let index = num; index >= 1; index -= 1) {
 }
 
 console.log(numFatorado);
+*/
+//2 item
+//Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
+
+let word = 'tryber';
+let wordInvert = ''
+
+for (let index = 0; index < word.length; index += 1) {
+    wordInvert += word[word.length - 1 - index];
+}
+
+console.log(wordInvert);
+console.log(word.length);
