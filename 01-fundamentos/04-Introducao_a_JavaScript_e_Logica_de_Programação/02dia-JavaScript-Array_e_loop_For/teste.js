b30345a37b82848dd8679857f655a97f95364e22
SilasPerillo <silasperillo@gmail.com>
2022-04-08 14:23:41 -0300

    commit da branch exercicio/4.3

---
+++ 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/02dia-JavaScript-Array_e_loop_For/teste.js
@@ -0,0 +1,20 @@
+/*
+let numbers = [5, 9, 5, 19, 70, 8, 100, 2, 37, 25];
+let impares = 0;
+
+for (let im = 0; im < numbers.length; im += 1) {
+    if (numbers[im] % 2 === 1){
+        impares += 1
+    }
+}
+
+    console.log("nenhum valor ímpar encontrado")
+console.log(impares) */
+
+let num = []
+
+for (let index = 1; index <= 25; index += 1 ) {
+    num.push(index)
+}
+    
+console.log(num)
