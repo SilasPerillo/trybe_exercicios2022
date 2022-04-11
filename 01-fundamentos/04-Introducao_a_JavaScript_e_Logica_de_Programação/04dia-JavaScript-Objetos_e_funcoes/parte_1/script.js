92e1a37ec6692ffb2d0652bab93b3657819e22dc
SilasPerillo <silasperillo@gmail.com>
2022-04-11 16:35:10 -0300

    Commit do exercicio/4.4 - 1 parte - item 5

--- 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/04dia-JavaScript-Objetos_e_funcoes/parte_1/script.js
+++ 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/04dia-JavaScript-Objetos_e_funcoes/parte_1/script.js
@@ -23,9 +23,23 @@ info['recorrente'] =  'Sim'
 // item 4
 //Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. 
 
-for (let value in info) {
-    console.log(info[value])
+//for (let value in info) {
+//    console.log(info[value])
+//
+//}
 
+// item 5
+//Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
 }
 
+for (let indexConcatenado in info) {
+    console.log(info[indexConcatenado], 'e', info2[indexConcatenado]);
+}
 
