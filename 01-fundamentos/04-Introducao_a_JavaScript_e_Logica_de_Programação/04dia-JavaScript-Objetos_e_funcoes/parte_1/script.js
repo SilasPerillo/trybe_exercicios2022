3190c22b5b471d37af2a617cde67273545751399
SilasPerillo <silasperillo@gmail.com>
2022-04-11 16:43:57 -0300

    Commit do exercicio/4.4 - 1 parte - item 6

--- 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/04dia-JavaScript-Objetos_e_funcoes/parte_1/script.js
+++ 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/04dia-JavaScript-Objetos_e_funcoes/parte_1/script.js
@@ -38,8 +38,25 @@ let info2 = {
     nota: 'O último MacPatinhas',
     recorrente: 'Sim',
 }
-
+/*
 for (let indexConcatenado in info) {
     console.log(info[indexConcatenado], 'e', info2[indexConcatenado]);
 }
+*/
+
+// item 6
+//Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+    },
+  };
+
+console.log("O livro favorito de", leitor.nome, leitor.sobrenome, "se chama '" + leitor.livrosFavoritos.titulo + "'")
 
