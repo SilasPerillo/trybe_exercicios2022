584527ee1493ec294a1174270c29763c92d1b431
SilasPerillo <silasperillo@gmail.com>
2022-04-11 16:03:26 -0300

    Commit do exercicio/4.4 - 1 parte - item 2

--- 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/04dia-JavaScript-Objetos_e_funcoes/parte_1/script.js
+++ 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/04dia-JavaScript-Objetos_e_funcoes/parte_1/script.js
@@ -7,9 +7,17 @@ let info = {
   //console.log('Bem-vinda ' + info.personagem)
 
 // item 2
-// Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+// Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. 
 
 info['recorrente'] =  'Sim'
 
-console.table(info)
+//console.table(info)
+
+// item 3
+//Faça um for/in que mostre todas as chaves do objeto. 
+
+for (lista in info) {
+    console.log(lista)
+}
+
 
