99f8ee1463db0f9767e45eb8ab6eb3cd8902612b
SilasPerillo <silasperillo@gmail.com>
2022-04-11 16:02:03 -0300

    Commit do exercicio/4.4 - 1 parte - item 2

--- 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/04dia-JavaScript-Objetos_e_funcoes/parte_1/script.js
+++ 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/04dia-JavaScript-Objetos_e_funcoes/parte_1/script.js
@@ -4,4 +4,12 @@ let info = {
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
 
-  console.log('Bem-vinda ' + info.personagem)
+  //console.log('Bem-vinda ' + info.personagem)
+
+// item 2
+// Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+
+info['recorrente'] =  'Sim'
+
+console.table(info)
+
