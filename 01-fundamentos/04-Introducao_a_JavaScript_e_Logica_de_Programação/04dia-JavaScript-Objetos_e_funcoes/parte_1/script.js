159e8b07467223667afa8d0f5de8fec23cd7204f
SilasPerillo <silasperillo@gmail.com>
2022-04-11 18:18:09 -0300

    Commit do exercicio/4.4 - 1 parte - item 7

--- 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/04dia-JavaScript-Objetos_e_funcoes/parte_1/script.js
+++ 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/04dia-JavaScript-Objetos_e_funcoes/parte_1/script.js
@@ -51,12 +51,25 @@ let leitor = {
     nome: 'Julia',
     sobrenome: 'Pessoa',
     idade: 21,
-    livrosFavoritos: {
+    livrosFavoritos:  [ { 
         titulo: 'O Pior Dia de Todos',
         autor: 'Daniela Kopsch',
         editora: 'Tordesilhas',
     },
+    ]
   };
 
-console.log("O livro favorito de", leitor.nome, leitor.sobrenome, "se chama '" + leitor.livrosFavoritos.titulo + "'")
+//console.log("O livro favorito de", leitor.nome, leitor.sobrenome, "se chama '" + leitor.livrosFavoritos.titulo + "'")
 
+// item 7
+//Adicione um novo livro favorito na chave livrosFavoritos , que é um array . Atribua a esta chave um objeto contendo as seguintes informações:
+
+leitor.livrosFavoritos.push(
+    {
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+    },
+);
+
+console.log(leitor.livrosFavoritos);
