dc4ed38af6cbe06d67b5e6ba569527ee081b827d
SilasPerillo <silasperillo@gmail.com>
2022-04-12 14:30:04 -0300

    Commit do exercicio/4.4 - 2 parte - item 2-2

--- 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/04dia-JavaScript-Objetos_e_funcoes/parte_2/script.js
+++ 01-fundamentos/04-Introducao_a_JavaScript_e_Logica_de_Programação/04dia-JavaScript-Objetos_e_funcoes/parte_2/script.js
@@ -1,7 +1,6 @@
 // 1 item
 // Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for.
 
-
 function palindromo(word) {
     for (let index = 0; index < word.length; index++) {      
         if (word[index] == word[(word.length - 1) - index]) {
@@ -32,4 +31,15 @@ function positionHigh (num) {
 
 
 //console.log(positionHigh([2, 3, 6, 7, 10, 1]))
+
+
+//3 item
+//Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+//Array de teste: [2, 4, 6, 7, 10, 0, -3]; posição 6
+
+
+function positionLower (numLower)
+    
+
+
 
