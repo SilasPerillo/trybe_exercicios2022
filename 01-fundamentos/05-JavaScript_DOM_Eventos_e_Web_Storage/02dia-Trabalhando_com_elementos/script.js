66b4a10448fa27a14db309c3341858ded5fdfae8
SilasPerillo <silasperillo@gmail.com>
2022-04-18 14:00:23 -0300

    exercicios ate II parte - item 4

--- 01-fundamentos/05-JavaScript_DOM_Eventos_e_Web_Storage/02dia-Trabalhando_com_elementos/script.js
+++ 01-fundamentos/05-JavaScript_DOM_Eventos_e_Web_Storage/02dia-Trabalhando_com_elementos/script.js
@@ -1,2 +1,4 @@
-let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
 console.log(elementoOndeVoceEsta);
+
+const body = document.getElementById('body');
