9fd52bea4ad5469a61c907111abaa93176d1073e
SilasPerillo <silasperillo@gmail.com>
2022-06-21 19:39:18 -0300

    criando pasta do pokedex no bloco 11

---
+++ 02-front_end/11-Componentes_com_estado-eventos-Forms/dia1-Componentes_com _estado_e_eventos/my-pokedex/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import ShowAllPokemons from './components/ShowAllPokemons'
+
+function App() {
+  return (
+    <div className="App">
+      <ShowAllPokemons />
+    </div>
+  );
+}
+
+export default App;
