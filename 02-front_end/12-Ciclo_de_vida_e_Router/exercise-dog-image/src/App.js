e0842ca153cb4cb8cdc881aadfee32c99613356e
SilasPerillo <silasperillo@gmail.com>
2022-07-18 13:37:43 -0300

    dia 18-jul

--- 02-front_end/12-Ciclo_de_vida_e_Router/exercise-dog-image/src/App.js
+++ 02-front_end/12-Ciclo_de_vida_e_Router/exercise-dog-image/src/App.js
@@ -1,29 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dogImg: '',
+      loanding: true,
+    };
+    this.fetchImg = this.fetchImg.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchImg();
+  }
+
+  shouldComponentUpdate(_nextProps, nextState) {
+    if (nextState.dogImg.includes('terrier')) {
+      return false;
+    }
+    return true;
+  }
+
+  componentDidUpdate() {
+    const { dogImg } = this.state;
+    localStorage.setItem('dogURL', dogImg);
+    const racaDog = dogImg.split('/')[4];
+    alert(racaDog);
+  }
+
+  fetchImg = () => {
+    this.setState(
+      { loanding: true },
+      () => (fetch('https://dog.ceo/api/breeds/image/random'))
+        .then((response) => response.json())
+        .then((data) => {
+          this.setState({
+            dogImg: data.message,
+            loanding: false,
+          });
+        }),
+    );
+  }
+
   render() {
+    const { dogImg, loanding } = this.state;
+    const loandingElement = <span>Carregando...</span>;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <p>
-            Edit
-            <code>
-              src/App.js
-            </code>
-            and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Imagem de dog Random</h1>
+        {
+          loanding
+            ? loandingElement
+            : (
+              <img
+                src={ dogImg }
+                alt="Imagem de dog Random"
+              />)
+        }
+        <button
+          type="button"
+          onClick={ this.fetchImg }
+        >
+          Novo doguinho
+        </button>
       </div>
     );
   }
